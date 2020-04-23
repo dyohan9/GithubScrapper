7158855e0c677b9bb0cb08f77cbe8f4c364a969d
Daniel Yohan <dyohan9@gmail.com>
2020-04-23 01:28:42 -0300

    Initial Commit

---
+++ github/spiders/github_spider.py
@@ -0,0 +1,92 @@
+import scrapy
+from scrapy import FormRequest
+
+
+class GithubSpider(scrapy.Spider):
+    name = "github_spider"
+    username_github = ""
+    password_github = ""
+
+    def save_file(self, response, filename, filemode="wb"):
+        with open(filename, filemode) as handle:
+            handle.write(response.body)
+        return filename
+
+    def get_absolute_url(self, path):
+        return f"https://github.com{path}"
+
+    def get_stargazers(self, repository):
+        return f"{self.get_absolute_url(repository)}/stargazers"
+
+    def start_requests(self):
+        yield scrapy.Request(
+            "https://github.com/login", meta={}, callback=self.parse_login
+        )
+
+    def parse_login(self, response):
+        yield FormRequest.from_response(
+            response,
+            url="https://github.com/session",
+            method="POST",
+            formdata={"login": self.username_github, "password": self.password_github},
+            callback=self.start_crawler,
+        )
+
+    def start_crawler(self, response):
+        yield scrapy.Request(
+            "https://github.com/search?q=rasa",
+            meta=response.meta,
+            callback=self.parse_repositories_page,
+        )
+
+    def parse_repositories_page(self, response):
+        page = response.xpath('//div/main/div/div/div/div/div/a[@class="next_page"]')
+        if page:
+            page = page.xpath("./@href").extract_first()
+            yield scrapy.Request(
+                self.get_absolute_url(page), meta=response.meta, callback=self.parse
+            )
+            yield scrapy.Request(
+                self.get_absolute_url(page),
+                meta=response.meta,
+                callback=self.parse_repositories_page,
+            )
+
+    def parse(self, response):
+        for i, li in enumerate(response.xpath("//div[4]/main/div/div[3]/div/ul/li"), 1):
+            repository = li.xpath("./div[2]/div[1]/a/@href").extract_first()
+            yield scrapy.Request(
+                self.get_stargazers(repository),
+                callback=self.parse_stargazers,
+                meta={"repository": repository},
+            )
+
+    def parse_stargazers(self, response):
+        for i, li in enumerate(response.xpath("//div/div/main/div/div/div/ol/li"), 1):
+            user_profile = li.xpath("./div/h3/span/a/@href").extract_first()
+            yield scrapy.Request(
+                self.get_absolute_url(user_profile),
+                callback=self.parse_profile,
+                meta=response.meta,
+            )
+
+        next_page = response.xpath(
+            "//div/div/main/div/div/div/div/div/*[contains"
+            '(@class, "btn btn-outline BtnGroup-item")][2]/@href'
+        ).extract_first()
+
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(
+                next_page, callback=self.parse_stargazers, meta=response.meta
+            )
+
+    def parse_profile(self, response):
+        email = response.xpath(
+            '//div/main/div/div/div/div/ul/li/a[@class="u-email "]/@href'
+        )
+        if len(email) > 0:
+            yield {
+                "name": "github",
+                "values": email.extract_first().replace("mailto:", ""),
+            }
